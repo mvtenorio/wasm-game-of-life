d9b589b85e3b5c26fa259b99be9d01f00b90c1d2
Marcus Tenório <marcusvtenorio@gmail.com>
2021-02-03 01:42:02 -0300

    Add a test and some docs

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
+    /// Creates a universe with the provided `width` and `height`.
     pub fn new(width: u32, height: u32) -> Universe {
         let size = (width * height) as usize;
         let mut cells = FixedBitSet::with_capacity(size);
@@ -34,38 +35,39 @@ impl Universe {
         }
     }
 
+    /// Get the width of the universe.
     pub fn width(&self) -> u32 {
         self.width
     }
 
+    /// Set the width of the universe.
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.reset_cells();
+    }
+
+    /// Get the height of the universe.
     pub fn height(&self) -> u32 {
         self.height
     }
 
+    /// Set the height of the universe.
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.reset_cells();
+    }
+
+    /// Get a pointer for the cells of the universe.
     pub fn cells(&self) -> *const u32 {
         self.cells.as_slice().as_ptr()
     }
 
+    /// Find the index for the cell given `row` and `col`
     #[wasm_bindgen(js_name = getIndex)]
     pub fn get_index(&self, row: u32, col: u32) -> usize {
         (row * self.width + col) as usize
-    }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-        count
     }
 
     /// Produces the next state based on the current state,
@@ -104,4 +106,50 @@ impl Universe {
         }
         self.cells = next;
     }
+
+    /// Resets all cells to the dead state.
+    pub fn reset_cells(&mut self) {
+        let size = (self.width * self.height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, false);
+        }
+        self.cells = cells;
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+
 }
