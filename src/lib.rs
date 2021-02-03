5d88d761e4fe91dc6a625a883ab74d55bc1fedf8
Marcus Tenório <marcusvtenorio@gmail.com>
2021-02-03 01:06:06 -0300

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,19 @@ impl Universe {
         count
     }
 
+    /// Produces the next state based on the current state,
+    /// according to the following rules:
+    ///
+    /// - Rule 1: Any live cell with fewer than two live neighbours
+    /// dies, as if caused by underpopulation.
+    /// - Rule 2: Any live cell with two or three live neighbours
+    /// lives on to the next generation.
+    /// - Rule 3: Any live cell with more than three live
+    /// neighbours dies, as if by overpopulation.
+    /// - Rule 4: Any dead cell with exactly three live neighbours
+    /// becomes a live cell, as if by reproduction.
+    ///
+    /// All other cells remain in the same state.
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -77,27 +90,16 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (true, x) if x < 2 => false,
-
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (true, 2) | (true, 3) => true,
-
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (true, x) if x > 3 => false,
-
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (false, 3) => true,
-
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-                next.set(idx, next_cell);
+                next.set(
+                    idx,
+                    match (cell, live_neighbors) {
+                        (true, x) if x < 2 => false,
+                        (true, 2) | (true, 3) => true,
+                        (true, x) if x > 3 => false,
+                        (false, 3) => true,
+                        (otherwise, _) => otherwise,
+                    },
+                );
             }
         }
         self.cells = next;
